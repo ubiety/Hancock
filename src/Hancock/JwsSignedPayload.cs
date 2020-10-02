08e7986509aa1f08c58048253c5cc85b00e37579
Dieter Lunn <coder2000@gmail.com>
2020-10-02 12:18:18 -0500

    docs: added comments for signed payload

--- src/Hancock/JwsSignedPayload.cs
+++ src/Hancock/JwsSignedPayload.cs
@@ -1,13 +1,28 @@
 namespace Hancock
 {
+    /// <summary>
+    ///     Signed JSON payload.
+    /// </summary>
     public class JwsSignedPayload
     {
+        /// <summary>
+        ///     Gets the unprotected JWS header.
+        /// </summary>
         public object Header { get; init; }
 
+        /// <summary>
+        ///     Gets the protected JWS header.
+        /// </summary>
         public string Protected { get; init; }
 
+        /// <summary>
+        ///     Gets the JWS payload.
+        /// </summary>
         public string Payload { get; init; }
 
+        /// <summary>
+        ///     Gets the JWS signature.
+        /// </summary>
         public string Signature { get; init; }
     }
 }
