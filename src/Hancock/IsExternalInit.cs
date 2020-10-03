2b780e5125b0d6fe2014792fb6ae81904a67be50
Dieter Lunn <coder2000@gmail.com>
2020-10-03 16:57:35 -0500

    fix: add temp class to resolve issue with preview feature

---
+++ src/Hancock/IsExternalInit.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Approved")]
+    public class IsExternalInit
+    {
+    }
+}
